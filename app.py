5f1362e74722887e4fa152cecd4f433a69903ed7
MPrevelato <mateusprevelato@hotmail.com>
2022-11-21 21:36:04 -0300

    random forest in streamlit

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from PIL import Image
-from sklearn.naive_bayes import GaussianNB
+from joblib import Parallel, delayed
+import joblib
+
 
 st.set_page_config(
     page_title="Crop Recommendation System",
@@ -25,15 +27,15 @@ st.markdown("<h5 style='text-align: center; color: black;'>Here you can insert t
 col1, col2, col3, col4, col5, col6, col7 = st.columns([1,1,4,1,4,1,1], gap = 'medium')
 
 with col3:
-    n_input = st.number_input('Insert N (kg/ha) value:', min_value= 0, max_value= 140)
-    p_input = st.number_input('Insert P (kg/ha) value:', min_value= 5, max_value= 145)
-    k_input = st.number_input('Insert K (kg/ha) value:', min_value= 5, max_value= 205)
-    temp_input = st.number_input('Insert Avg Temperature (ºC) value:', min_value= 9., max_value= 43., step = 1., format="%.2f")
+    n_input = st.number_input('Insert N (kg/ha) value:', min_value= 0, max_value= 140, help = 'Insert here the Nitrogen density (kg/ha) from 0 to 140.')
+    p_input = st.number_input('Insert P (kg/ha) value:', min_value= 5, max_value= 145, help = 'Insert here the Phosphorus density (kg/ha) from 5 to 145.')
+    k_input = st.number_input('Insert K (kg/ha) value:', min_value= 5, max_value= 205, help = 'Insert here the Potassium density (kg/ha) from 5 to 205.')
+    temp_input = st.number_input('Insert Avg Temperature (ºC) value:', min_value= 9., max_value= 43., step = 1., format="%.2f", help = 'Insert here the Avg Temperature (ºC) from 9 to 43.')
 
 with col5:
-    hum_input = st.number_input('Insert Avg Humidity (%) value:', min_value= 15., max_value= 99., step = 1., format="%.2f")
-    ph_input = st.number_input('Insert pH value:', min_value= 3.6, max_value= 9.9, step = 0.1, format="%.2f")
-    rain_input = st.number_input('Insert Avg Rainfall (mm) value:', min_value= 21.0, max_value= 298.0, step = 0.1, format="%.2f")
+    hum_input = st.number_input('Insert Avg Humidity (%) value:', min_value= 15., max_value= 99., step = 1., format="%.2f", help = 'Insert here the Avg Humidity (%) from 15 to 99.')
+    ph_input = st.number_input('Insert pH value:', min_value= 3.6, max_value= 9.9, step = 0.1, format="%.2f", help = 'Insert here the pH from 3.6 to 9.9')
+    rain_input = st.number_input('Insert Avg Rainfall (mm) value:', min_value= 21.0, max_value= 298.0, step = 0.1, format="%.2f", help = 'Insert here the Avg Rainfall (mm) from 21 to 298')
 
 
 df = pd.read_csv('Crop_recommendation.csv')
@@ -41,21 +43,17 @@ df = pd.read_csv('Crop_recommendation.csv')
 X = df.drop('label', axis = 1)
 y = df['label']
 
+rdf_clf = joblib.load('rdf_clf.pkl')
 
-gnb = GaussianNB()
-gnb.fit(X,y)
 
 predict_inputs = [[n_input,p_input,k_input,temp_input,hum_input,ph_input,rain_input]]
-
-
-
 
 with col5:
     predict_btn = st.button('Get Your Prediction')
 
 if predict_btn:
-    gnb_predicted_value = gnb.predict(predict_inputs)
-    st.text('Crop suggestion: {}'.format(gnb_predicted_value[0]))
+    rdf_predicted_value = rdf_clf.predict(predict_inputs)
+    st.text('Crop suggestion: {}'.format(rdf_predicted_value[0]))
         
-    df_pred = df[df['label'] == gnb_predicted_value[0]]
+    df_pred = df[df['label'] == rdf_predicted_value[0]]
     st.dataframe(df_pred.describe(), use_container_width=True)
